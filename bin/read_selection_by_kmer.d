4b42543f30d80bf12fa6c23402264f65cc153552
Ben J Woodcroft <donttrustben@gmail.com>
2013-05-20 20:25:23 +1000

    changes

--- bin/read_selection_by_kmer.d
+++ bin/read_selection_by_kmer.d
@@ -4,36 +4,51 @@ import std.stdio;
 import std.string;
 import std.conv;
 import std.getopt;
+import std.file;
+import std.array;
+import bio.core.fasta;
 
 void main(string[] args){
-  string whitelistFile, blacklistFile = null;
+  string whitelistFile, blacklistFile, fastaFile = null;
   bool help = false;
   getopt(args,
     "whitelist",  &whitelistFile,
     "blacklist",  &blacklistFile,
+    "reads",  &fastaFile,
     "help|h",         &help,
   );
   if(help){
     writeln("my helpa");
   }
-  if(whitelistFile == null){stderr.writeln("Need to specify a newline-separeted list of whitelisted kmers as --whitelist <file>");}
+  else if(whitelistFile is null){stderr.writeln("Error: Need to specify a newline-separeted list of whitelisted kmers as --whitelist <file>");}
+  else if(fastaFile is null){stderr.writeln("Error: Need to specify a fasta file of reads to work with as --reads <fasta_file>");}
+  else {
 
   //read in a text file of kmers that we wish to find, the whitelist
-  string[] whitelistKmers = new string[];
-  int i = 0;
-  foreach(line; File(whitelistFile).byLine()){
-  writeln(line);
-  writeln(i);
-    whitelistKmers[i] = to!string(line);
-    i += 1;
-  }
-  stderr.writefln("Read in %i kmers as a whitelist, e.g. %s",whitelistKmers.length,whitelistKmers[0]);
+  string[] whitelist = split(cast(string) read(whitelistFile));
+  stderr.writeln("Read in ",whitelist.length," kmers as a whitelist, e.g. ",whitelist.front);
 
   //if blacklistFile is specified, read in a list of kmers that are blacklisted, otherwise make an empty array
+  string[] blacklist = [];
+  if(blacklistFile != null){
+    blacklist = split(cast(string) read(blacklistFile));
+    stderr.writeln("Read in ",blacklist.length," blacklisted kmers e.g. ",blacklist[0]);
+  } else {
+    stderr.writeln("No blacklisted kmers specified");
+  }
 
   //Iterate through the fastq reads given.
-  //If they contain one of the blacklist kmers, then go to next
+  auto fastas = fastaRecords(fastaFile);
+  writeln(fastas);
+  writeln(fastas.front);
+  writeln(fastas.front);
+  writeln(fastas.front.sequence)
+
+  //nucleotideSequence(window, true) //reverse complement too
+  foreach(seq; fastas.popFront)
+  if(fasta)
+  //If they contain one of the blacklist kmers, then skip
   //How many of each whitelist kmers are found (including in the reverse complement)?
   //Remove from the whitelist any kmers where the maximal allowed number of kmers has already been reached
 }
-
+}
