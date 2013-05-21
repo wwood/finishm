56bfbb22ac94e98c4ad8f07903599c2a748f56dd
Ben J. Woodcroft <donttrustben near gmail.com>
2013-05-21 16:58:44 +1000

    tested bin/read_selection_by_kmer.d script

--- bin/read_selection_by_kmer.d
+++ bin/read_selection_by_kmer.d
@@ -7,14 +7,20 @@ import std.getopt;
 import std.file;
 import std.array;
 import bio.core.fasta;
+import bio.core.sequence;
+import std.algorithm;
 
 void main(string[] args){
   string whitelistFile, blacklistFile, fastaFile = null;
-  bool help = false;
+  bool help, verbose, quiet = false;
+  int targetPerKmer = 1;
   getopt(args,
     "whitelist",  &whitelistFile,
     "blacklist",  &blacklistFile,
     "reads",  &fastaFile,
+    "kmer-coverage-target", &targetPerKmer,
+    "verbose", &verbose,
+    "quiet", &quiet,
     "help|h",         &help,
   );
   if(help){
@@ -23,32 +29,88 @@ void main(string[] args){
   else if(whitelistFile is null){stderr.writeln("Error: Need to specify a newline-separeted list of whitelisted kmers as --whitelist <file>");}
   else if(fastaFile is null){stderr.writeln("Error: Need to specify a fasta file of reads to work with as --reads <fasta_file>");}
   else {
+    if(verbose) quiet=false;
 
   //read in a text file of kmers that we wish to find, the whitelist
   string[] whitelist = split(cast(string) read(whitelistFile));
-  stderr.writeln("Read in ",whitelist.length," kmers as a whitelist, e.g. ",whitelist.front);
+  if (verbose)
+    stderr.writeln("Read in ",whitelist.length," kmers as a whitelist, e.g. ",whitelist.front);
 
   //if blacklistFile is specified, read in a list of kmers that are blacklisted, otherwise make an empty array
   string[] blacklist = [];
   if(blacklistFile != null){
     blacklist = split(cast(string) read(blacklistFile));
-    stderr.writeln("Read in ",blacklist.length," blacklisted kmers e.g. ",blacklist[0]);
+    if(verbose)
+      stderr.writeln("Read in ",blacklist.length," blacklisted kmers e.g. ",blacklist[0]);
   } else {
-    stderr.writeln("No blacklisted kmers specified");
+    if(verbose)
+      stderr.writeln("No blacklisted kmers specified");
   }
+
+  int[] whitelistCounts;
+  whitelistCounts.length = whitelist.length;
 
   //Iterate through the fastq reads given.
   auto fastas = fastaRecords(fastaFile);
-  writeln(fastas);
-  writeln(fastas.front);
-  writeln(fastas.front);
-  writeln(fastas.front.sequence)
+  bool all_accounted_for = false;
+  foreach(seq; fastas){
+    if (verbose)
+      stderr.writeln("Inspecting ", seq);
+    //If they contain one of the blacklist kmers, then skip
+    string fwd = seq.sequence;
+    string rev = to!string(nucleotideSequence(fwd, true));
+
+    //I'm sure there is a faster way to search for an array of strings within a particular string, but eh for now.
+    bool blacklisted = false;
+    foreach(black; blacklist){
+      if (std.string.indexOf(fwd, black) != -1 || std.string.indexOf(rev, black) != -1){
+        //blacklisted kmer found
+        blacklisted = true;
+        break;
+      }
+    }
+    if(blacklisted){
+      if(verbose)
+        stderr.writeln(fwd," contains a blacklisted kmer, not including this one");
+      continue;
+    } else {
+      if(verbose)
+        stderr.writeln(fwd," not blacklisted");
+    }
+
+    //How many of each whitelist kmers are found (including in the reverse complement)?
+    bool whitelisted = false;
+    foreach(i, white; whitelist){
+      //don't look for whitelisted kmers
+      if (whitelistCounts[i] < targetPerKmer){
+        if (std.string.indexOf(fwd, white) != -1 || std.string.indexOf(rev, white) != -1){
+          if(verbose)
+            stderr.writeln("Conforms to whitelist entry #",i);
+          whitelisted = true;
+          whitelistCounts[i] += 1;
+
+          //check that all whitelist counts have not been overflowed.
+          if(!quiet)
+            stderr.writeln("kmer index ",i," now accounted for");
+          if(minCount(whitelistCounts)[0] >= targetPerKmer){
+            if(verbose)
+              stderr.writeln("All whitelisted kmers now accounted for");
+            all_accounted_for = true; //all done, no more fasta entries required
+          }
+        }
+      }
+    }
+    //print this sequence if it passes the whitelist test
+    if (whitelisted){
+      writeln(">", seq.header);
+      writeln(fwd);
+    }
+
+    if(all_accounted_for) break;
+  }
 
   //nucleotideSequence(window, true) //reverse complement too
-  foreach(seq; fastas.popFront)
-  if(fasta)
-  //If they contain one of the blacklist kmers, then skip
-  //How many of each whitelist kmers are found (including in the reverse complement)?
-  //Remove from the whitelist any kmers where the maximal allowed number of kmers has already been reached
+
+  //if(fasta)
 }
 }
---
+++ spec/read_selection_by_kmer_spec.rb
@@ -0,0 +1,103 @@
+require 'tempfile'
+require 'rspec'
+require 'pp'
+require 'systemu'
+
+# To run this test:
+# $ rspec /path/to/test_script_being_tested.rb
+
+# Assumes that the name of the file being tested is ../something.rb relative to the directory containing this test scripts, and the name of this tes script is test_something.rb
+$:.unshift File.join(File.dirname(__FILE__),'..')
+script_under_test = File.basename(__FILE__).gsub(/^test_/,'')
+base = File.join File.dirname(__FILE__),'..'
+path_to_script = "rdmd -I#{base}/../BioD/ #{base}/bin/read_selection_by_kmer.d"
+
+# Re-build at the start
+status, stdout, stderr = systemu "rdmd --build-only -I#{base}/../BioD/ #{base}/bin/read_selection_by_kmer.d"
+raise stderr unless stderr == ""
+
+describe script_under_test do
+  it 'should scripting test ok' do
+    reads = %w(>whitelist_me ATGCCCC >blacklist_me ATGCATGG >ignore_me AAAAAAAA)
+    whitelist = %w(ATGC)
+    blacklist = %w(ATGG)
+
+    reads_file = Tempfile.new('reads'); reads_file.puts reads.join("\n"); reads_file.close
+    whitelist_file = Tempfile.new('whitelist'); whitelist_file.puts whitelist.join("\n"); whitelist_file.close
+    blacklist_file = Tempfile.new('blacklist'); blacklist_file.puts blacklist.join("\n"); blacklist_file.close
+    status, stdout, stderr = systemu "#{path_to_script} --whitelist #{whitelist_file.path} --blacklist #{blacklist_file.path} --reads #{reads_file.path} --kmer-coverage-target 10"
+    raise stderr unless stderr == ""
+    status.exitstatus.should eq(0)
+    stdout.should eq(%w(>whitelist_me ATGCCCC).join("\n")+"\n")
+  end
+
+  it 'should work without blacklist' do
+    reads = %w(>whitelist_me ATGCCCC >blacklist_me ATGCATGG >ignore_me AAAAAAAA)
+    whitelist = %w(ATGC)
+    blacklist = %w()
+
+    reads_file = Tempfile.new('reads'); reads_file.puts reads.join("\n"); reads_file.close
+    whitelist_file = Tempfile.new('whitelist'); whitelist_file.puts whitelist.join("\n"); whitelist_file.close
+    blacklist_file = Tempfile.new('blacklist'); blacklist_file.puts blacklist.join("\n"); blacklist_file.close
+    status, stdout, stderr = systemu "#{path_to_script} --whitelist #{whitelist_file.path} --reads #{reads_file.path} --kmer-coverage-target 10"
+    raise stderr unless stderr == ""
+    status.exitstatus.should eq(0)
+    stdout.should eq(%w(>whitelist_me ATGCCCC >blacklist_me ATGCATGG).join("\n")+"\n")
+  end
+
+  it 'should default to a low kmer coverage' do
+    reads = %w(>whitelist_me ATGCCCC >blacklist_me ATGCATGG >ignore_me AAAAAAAA)
+    whitelist = %w(ATGC)
+    blacklist = %w()
+
+    reads_file = Tempfile.new('reads'); reads_file.puts reads.join("\n"); reads_file.close
+    whitelist_file = Tempfile.new('whitelist'); whitelist_file.puts whitelist.join("\n"); whitelist_file.close
+    blacklist_file = Tempfile.new('blacklist'); blacklist_file.puts blacklist.join("\n"); blacklist_file.close
+    status, stdout, stderr = systemu "#{path_to_script} --whitelist #{whitelist_file.path} --reads #{reads_file.path}"
+    raise stderr unless stderr == ""
+    status.exitstatus.should eq(0)
+    stdout.should eq(%w(>whitelist_me ATGCCCC).join("\n")+"\n")
+  end
+
+  it 'should handle multiple whitelisted components' do
+    reads = %w(>whitelist_me ATGCCCC >blacklist_me ATGCATGG >ignore_me AAAAAAAA)
+    whitelist = %w(ATGC AAAA)
+    blacklist = %w(ATGG)
+
+    reads_file = Tempfile.new('reads'); reads_file.puts reads.join("\n"); reads_file.close
+    whitelist_file = Tempfile.new('whitelist'); whitelist_file.puts whitelist.join("\n"); whitelist_file.close
+    blacklist_file = Tempfile.new('blacklist'); blacklist_file.puts blacklist.join("\n"); blacklist_file.close
+    status, stdout, stderr = systemu "#{path_to_script} --whitelist #{whitelist_file.path} --blacklist #{blacklist_file.path} --reads #{reads_file.path}"
+    raise stderr unless stderr == ""
+    status.exitstatus.should eq(0)
+    stdout.should eq(%w(>whitelist_me ATGCCCC >ignore_me AAAAAAAA).join("\n")+"\n")
+  end
+
+  it 'should handle multiple whitelisted components in the same sequence' do
+    reads = %w(>whitelist_me ATGCCCCAAAA >blacklist_me ATGCATGG >ignore_me AAAAAAAA)
+    whitelist = %w(ATGC AAAA)
+    blacklist = %w(ATGG)
+
+    reads_file = Tempfile.new('reads'); reads_file.puts reads.join("\n"); reads_file.close
+    whitelist_file = Tempfile.new('whitelist'); whitelist_file.puts whitelist.join("\n"); whitelist_file.close
+    blacklist_file = Tempfile.new('blacklist'); blacklist_file.puts blacklist.join("\n"); blacklist_file.close
+    status, stdout, stderr = systemu "#{path_to_script} --whitelist #{whitelist_file.path} --blacklist #{blacklist_file.path} --reads #{reads_file.path}"
+    raise stderr unless stderr == ""
+    status.exitstatus.should eq(0)
+    stdout.should eq(%w(>whitelist_me ATGCCCCAAAA).join("\n")+"\n")
+  end
+
+  it 'should handle multiple whitelisted components in the same sequence and one in revcomp' do
+    reads = %w(>whitelist_me ATGCCCCTTTT >blacklist_me ATGCATGG >ignore_me AAAAAAAA)
+    whitelist = %w(ATGC AAAA)
+    blacklist = %w(ATGG)
+
+    reads_file = Tempfile.new('reads'); reads_file.puts reads.join("\n"); reads_file.close
+    whitelist_file = Tempfile.new('whitelist'); whitelist_file.puts whitelist.join("\n"); whitelist_file.close
+    blacklist_file = Tempfile.new('blacklist'); blacklist_file.puts blacklist.join("\n"); blacklist_file.close
+    status, stdout, stderr = systemu "#{path_to_script} --whitelist #{whitelist_file.path} --blacklist #{blacklist_file.path} --reads #{reads_file.path}"
+    raise stderr unless stderr == ""
+    status.exitstatus.should eq(0)
+    stdout.should eq(%w(>whitelist_me ATGCCCCTTTT).join("\n")+"\n")
+  end
+end
