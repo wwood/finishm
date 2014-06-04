cbecc09b7d9fe21de3df1969b9d7a23c7de72ed6
Ben Woodcroft <donttrustben@gmail.com>
2014-06-04 22:39:25 +1000

    begin single recoherent wandering

---
+++ lib/assembly/single_coherent_wanderer.rb
@@ -0,0 +1,8 @@
+class Bio::AssemblyGraphAlgorithms::SingleCoherentPathsBetweenNodesFinder
+  include Bio::FinishM::Logging
+
+  # Like AcyclicConnectionFinder#depth_first_search_with_leash except use
+  # single read recoherence.
+  def wander(finishm_graph, leash_length, recoherence_kmer)
+  end
+end
