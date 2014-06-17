dcb60ae9d954bc7afa7159148ea198c753b2bd4b
Ben Woodcroft <donttrustben@gmail.com>
2014-06-17 18:57:20 +1000

    oops

---
+++ lib/assembly/hybrid_velvet_graph.rb
@@ -0,0 +1,48 @@
+# A velvet Graph where the nodes and arcs are in Ruby, but the NodedReads are in C
+class Bio::FinishM::HybridGraph
+  include Bio::FinishM::Logging
+
+  def initialize(bio_velvet_graph, bio_velvet_underground_graph)
+    @bio_velvet_graph = bio_velvet_graph
+    @bio_velvet_underground_graph = bio_velvet_underground_graph
+
+    @nodes = NodeArray.new(bio_velvet_graph, bio_velvet_underground_graph)
+  end
+
+  def method_missing(method_sym, *args, &block)
+    @bio_velvet_graph.send(method_sym, *args, &block)
+  end
+
+  class NodeArray
+    include Enumerable
+
+    def initialize(bio_velvet_graph, bio_velvet_underground_graph)
+      @bio_velvet_graph = bio_velvet_graph
+      @bio_velvet_underground_graph = bio_velvet_underground_graph
+    end
+
+    def []=(node_id, value)
+      raise "method not implemented"
+    end
+
+    def [](node_id)
+      bio_velvet_node = @bio_velvet_graph.nodes[node_id]
+      bio_velvet_node.short_reads = @bio_velvet_underground_graph.nodes[node_id].short_reads
+      return bio_velvet_node
+    end
+
+    def delete(node)
+      raise "method not implemented"
+    end
+
+    def length
+      @bio_velvet_graph.nodes.length
+    end
+
+    def each(&block)
+      @bio_velvet_graph.nodes.each do |node|
+        block.yield self[node.node_id]
+      end
+    end
+  end
+end
