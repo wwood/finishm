538d66ded669d745c3add1f129f9600ec59d0e76
Tim Lamberton <t.lamberton@uq.edu.au>
2015-04-17 16:01:24 +1000

    Added trail_stats method to Dijkstra class to count trails aligning at distances from origin node.

--- lib/assembly/dijkstra.rb
+++ lib/assembly/dijkstra.rb
@@ -185,7 +185,7 @@ class Bio::AssemblyGraphAlgorithms::Dijkstra
   end
 
   # Generate an array of OrientedTrails
-  def alignments(graph, initial_oriented_node, options={})
+  def all_trails(graph, initial_oriented_node, options={})
     pqueue = DS::AnyPriorityQueue.new {|a,b| a < b}
     first = DistancedOrientedNodeTrail.new
     first_trail = OrientedNodeTrail.new
@@ -292,9 +292,236 @@ class Bio::AssemblyGraphAlgorithms::Dijkstra
     return to_return.map{|d| d.oriented_trail}
   end
 
+  # Returns a Hash of [node_id, first_side] => [position_distance] => position_multiplicity
+  def trail_stats(graph, initial_oriented_node, options={})
+    counter = DistancedNodeCounter.new
+    first = DistancedOrientedNode.new
+    first.node = initial_oriented_node.node
+    first.first_side = initial_oriented_node.first_side
+    first.positions = Hash.new 1
+    first.positions[0] = 1
+    first.distance = 0
+    counter.push first
+
+    first_node = true
+    found_nodes = Set.new([first.node.node_id])
+
+    while min_distanced_node = counter.shift
+
+      log.debug "Working from #{min_distanced_node.inspect}" if log.debug?
+      log.debug "Node currently maps to the following distances: #{min_distanced_node.positions.to_a.collect{|d| d.join(': ')}.join(', ')}." if log.debug?
+
+      if options[:leash_length] and min_distanced_node.distance > options[:leash_length]
+        # we are passed leash length, and this is the nearest node. So we are finito.
+        log.debug "passed the leash length, cutting short our travels" if log.debug?
+        next
+      end
+
+      if options[:max_nodes] and found_nodes.length > options[:max_nodes]
+        log.debug "passed max-nodes threshold and have #{found_nodes.length} nodes" if log.debug?
+        # remove extras that may have been queued if we are over the limit
+        next
+      end
+
+
+      # Queue nodes after this one
+      current_distance = min_distanced_node.distance
+      current_positions = min_distanced_node.positions
+
+      # Find neighbouring nodes
+      neighbours = nil
+      if options[:neighbour_finder]
+        neighbours = options[:neighbour_finder].neighbours(min_distanced_node)
+      else
+        neighbours = min_distanced_node.next_neighbours(graph)
+      end
+
+      # explore each neighbour node
+      neighbours.each do |onode|
+        found_nodes << onode.node.node_id
+        length_to_new = 0
+        if options[:neighbour_finder]
+          # Don't use negative distances as this algorithm cannot handle it, and it is impossible
+          # anyway
+          if onode.distance > 0
+            length_to_new += onode.distance
+          else
+            length_to_new += 0
+          end
+        end
+        unless first_node
+          length_to_new += min_distanced_node.node.length_alone
+        end
+        new_distance = current_distance + length_to_new
+        new_positions = {}
+        current_positions.each_pair do |distance, multiplicity|
+          new_positions[distance + length_to_new] = multiplicity
+        end
+
+        log.debug "Queuing new distance for neighbour: #{onode}: #{new_distance}" if log.debug?
+        # new shortest distance found. queue it up
+        distanced_node = DistancedOrientedNode.new
+        distanced_node.node = onode.node
+        distanced_node.first_side = onode.first_side
+        distanced_node.distance = new_distance
+        distanced_node.positions = new_positions
+        counter.push distanced_node
+
+        if options[:ignore_directions]
+          log.debug "Queuing new distance for reversed neighbour: #{onode.reverse}: #{new_distance}" if log.debug?
+          reverse = DistancedOrientedNode.new
+          reverse.node = onode.node
+          reverse.first_side = onode.reverse.first_side
+          reverse.distance = new_distance
+          reverse.positions = new_positions
+          counter.push reverse
+        end
+      end
+
+      first_node = false
+    end
+
+    to_return = counter.visited_multiplicity_at_position
+
+    if options[:max_nodes] and found_nodes.length > options[:max_nodes]
+      distances_direction_agnostic = {}
+      to_return.each do |key, positions|
+        prev = distances_direction_agnostic[key[0]]
+        distance = positions.keys.min
+        if prev.nil? or prev > distance
+          distances_direction_agnostic[key[0]] = distance
+        end
+      end
+      if distances_direction_agnostic.length > options[:max_nodes]
+        sorted = distances_direction_agnostic.to_a.sort{|a,b| a[1]<=>b[1]}
+        # deal with ties i.e. at the edge there can be multiple neighbours
+        last_distance = sorted[options[:max_nodes]][1]
+
+        # only keep those nodes that are sufficiently close
+        to_return.select! do |key, positions|
+          positions.keys.min <= last_distance
+        end
+      end
+    end
+
+    # if ignore directions, then fixup the return so that each direction is included
+    if options[:ignore_directions]
+      new_to_return = {}
+      log.debug "Computing directionless distances for #{to_return}." if log.debug?
+      to_return.keys.collect{|key| key[0]}.uniq.each do |node_id|
+        keys = [
+          Bio::Velvet::Graph::OrientedNodeTrail::START_IS_FIRST,
+          Bio::Velvet::Graph::OrientedNodeTrail::END_IS_FIRST].collect do |direction|
+            [node_id, direction]
+          end
+        log.debug "Merging distances for keys #{keys}." if log.debug?
+        new_positions = keys.collect{|k| to_return[k]}.reject{|pn| pn.nil?}.reduce do |memo, positions|
+          log.debug "Adding directionless distances from #{positions} to #{memo}." if log.debug?
+          positions.each_pair do |distance, multiplicity|
+            if memo.has_key? distance
+              memo[distance] += multiplicity #add ways to get to node at position
+            else
+              memo[distance] = multiplicity
+            end
+          end
+          memo
+        end
+        log.debug "Distances merged: #{new_positions}." if log.debug?
+        keys.each do |key|
+          new_to_return[key] = new_positions
+        end
+      end
+      to_return = new_to_return
+    end
+
+    return counter.by_position(to_return)
+  end
+
+  # Queue for DistancedOrientedNodes that prioritises nodes by shortest seen distance to node.
+  # DistancedOrientedNodes can contain 'position' attribute with a Hash [distance]=>multiplicity which
+  # describes a mapping of the start of the node to a position at a set "distance" from an origin node, as
+  # well as a "multiplicity" of the mapping: the number of possible paths containing that node at the
+  # position.
+  # DistancedNodeCounter tracks positions for both visited node, as well as currently queued node,
+  # and merges positions of newly pushed nodes. If duplicate nodes are pushed for a current node, the queued
+  # node distance and positions are updated when dequeuing.
+  # Algorithm is similar to the 'train' algorithm used for the
+  # Bio::AssemblyGraphAlgorithms::BubblyAssembler::Bubble class, with stations counting the number of carriages
+  # seen.
+  class DistancedNodeCounter
+    #include Bio::FinishM::Logging
+    attr_reader :visited_multiplicity_at_position
+
+    def initialize
+      @visited_multiplicity_at_position = {}
+      @current_multiplicity_at_position = {}
+      @pqueue = DS::AnyPriorityQueue.new {|a,b| a < b}
+    end
+
+    def push(dnode)
+      # merge position hash for visited node
+      visited_positions = @visited_multiplicity_at_position[dnode.to_settable] || {}
+      dnode.positions.each_pair do |distance, multiplicity|
+        if visited_positions.has_key? distance
+          visited_positions[distance] += multiplicity #found m new paths to this position
+          #log.debug "Visited node #{dnode.node.node_id} already found at distance #{distance}. Updating multiplicity to #{visited_positions[distance]}." if log.debug?
+        else
+          visited_positions[distance] = multiplicity
+          #log.debug "Visited node #{dnode.node.node_id} found at new distance #{distance}, with multiplicity #{multiplicity}." if log.debug?
+        end
+      end
+      @visited_multiplicity_at_position[dnode.to_settable] = visited_positions
+
+      if @current_multiplicity_at_position.has_key? dnode.to_settable
+        #log.debug "Visited node #{dnode.node.node_id} is already queued." if log.debug?
+        # node already queued, merge position hash for current node
+        current_positions = @current_multiplicity_at_position[dnode.to_settable]
+        dnode.positions.each_pair do |distance, multiplicity|
+          if current_positions.has_key? distance
+            current_positions[distance] += multiplicity
+            #log.debug "Queued node already found at distance #{distance}. Updating multiplicity to #{current_positions[distance]}." if log.debug?
+          else
+            current_positions[distance] = multiplicity
+            #log.debug "Queued node found at new distance #{distance}, with multiplicity #{multiplicity}." if log.debug?
+          end
+        end
+      else
+        # add position hash and queue node
+        @current_multiplicity_at_position[dnode.to_settable] = dnode.positions
+        @pqueue.push(dnode, shortest_distance(dnode))
+      end
+    end
+
+    def shortest_distance(dnode)
+      raise unless @visited_multiplicity_at_position.has_key? dnode.to_settable
+      @visited_multiplicity_at_position[dnode.to_settable].keys.min
+    end
+
+    def shift
+      dnode = @pqueue.shift
+      unless dnode.nil?
+        dnode.positions = @current_multiplicity_at_position.delete dnode.to_settable
+        dnode.distance = shortest_distance(dnode) #minimum distance to node
+      end
+      return dnode
+    end
+
+    def by_position(visited_multiplicity_at_position)
+      nodes_at_position = {}
+      visited_multiplicity_at_position.each_pair do |key, positions|
+        positions.each_pair do |distance, multiplicity|
+          node_hash = nodes_at_position[distance] || {}
+          node_hash[key] = multiplicity
+          nodes_at_position[distance] = node_hash
+        end
+      end
+      return nodes_at_position
+    end
+  end
+
   # An oriented node some distance from the origin of exploration
   class DistancedOrientedNode
-    attr_accessor :node, :first_side, :distance
+    attr_accessor :node, :first_side, :distance, :positions
 
     # Using Set object, often we want two separate objects to be considered equal even if
     # they are distinct objects
--- spec/dijkstra_spec.rb
+++ spec/dijkstra_spec.rb
@@ -1,139 +1,303 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Dijkstra" do
+  describe 'min distances' do
 
-  "SOME OF THIS IS LIKELY TESTED IN OTHER FILES SO GO LOOKING BEFORE NEW TESTS ARE WRITTEN"
+    "SOME OF THIS IS LIKELY TESTED IN OTHER FILES SO GO LOOKING BEFORE NEW TESTS ARE WRITTEN"
 
-  it 'should do hello world' do
-    f = GraphTesting.finishm_graph([[1,2],[3,4]])
-    GraphTesting.add_reads_to_nodes(f, [[1,1],[2,1],[3,2]])
-    GraphTesting.make_reads_paired(f)
-    dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
-    onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
-    # won't find node 2 because the default finder is single-ended
-    dijkstra.min_distances(f.graph, onode).should == {[1, :start_is_first]=>0, [2, :start_is_first]=>0}
-  end
+    it 'should do hello world' do
+      f = GraphTesting.finishm_graph([[1,2],[3,4]])
+      GraphTesting.add_reads_to_nodes(f, [[1,1],[2,1],[3,2]])
+      GraphTesting.make_reads_paired(f)
+      dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
+      onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
+      # won't find node 2 because the default finder is single-ended
+      dijkstra.min_distances(f.graph, onode).should == {[1, :start_is_first]=>0, [2, :start_is_first]=>0}
+    end
 
-  it 'should do paired end jumps' do
-    f = GraphTesting.finishm_graph([[1,2],[3,4]])
-    GraphTesting.add_reads_to_nodes(f, [[1,1],[2,1],[3,2],[4,2]])
-    GraphTesting.make_reads_paired(f)
-    dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
-    onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
-    finder = Bio::FinishM::PairedEndNeighbourFinder.new(f, 100)
+    it 'should do paired end jumps' do
+      f = GraphTesting.finishm_graph([[1,2],[3,4]])
+      GraphTesting.add_reads_to_nodes(f, [[1,1],[2,1],[3,2],[4,2]])
+      GraphTesting.make_reads_paired(f)
+      dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
+      onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
+      finder = Bio::FinishM::PairedEndNeighbourFinder.new(f, 100)
 
-    dijkstra.min_distances(f.graph, onode, :neighbour_finder => finder
-      ).should == {[1, :start_is_first]=>0,
+      dijkstra.min_distances(f.graph, onode, :neighbour_finder => finder
+        ).should == {[1, :start_is_first]=>0,
+          [2, :start_is_first]=>0,
+          [3, :end_is_first] => 80.0,
+          [4, :end_is_first] => 80.0}
+    end
+
+    it 'should respect max-nodes' do
+      f = GraphTesting.finishm_graph([[1,2],[2,3],[3,4]])
+      dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
+      onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
+      dijkstra.min_distances(f.graph, onode).should == {
+        [1, :start_is_first]=>0,
         [2, :start_is_first]=>0,
-        [3, :end_is_first] => 80.0,
-        [4, :end_is_first] => 80.0}
+        [3, :start_is_first]=>10,
+        [4, :start_is_first]=>20,
+        }
+      dijkstra.min_distances(f.graph, onode, :max_nodes => 2).should == {
+        [1, :start_is_first]=>0,
+        [2, :start_is_first]=>0,
+        [3, :start_is_first]=>10,
+        }
+      dijkstra.min_distances(f.graph, onode, :max_nodes => 0).should == {
+        [1, :start_is_first]=>0,
+        }
+      dijkstra.min_distances(f.graph, onode, :max_nodes => 3).should == {
+        [1, :start_is_first]=>0,
+        [2, :start_is_first]=>0,
+        [3, :start_is_first]=>10,
+        [4, :start_is_first]=>20,
+        }
+      dijkstra.min_distances(f.graph, onode, :max_nodes => 30).should == {
+        [1, :start_is_first]=>0,
+        [2, :start_is_first]=>0,
+        [3, :start_is_first]=>10,
+        [4, :start_is_first]=>20,
+        }
+    end
+
+    it 'should max-nodes when ignoring directions' do
+      f = GraphTesting.finishm_graph([[1,2],[2,3],[3,4]])
+      dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
+      onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
+
+      dijkstra.min_distances(f.graph, onode, {:max_nodes => 0, :ignore_directions => true}).should == {
+        [1, :start_is_first]=>0,
+        [1, :end_is_first]=>0,
+        }
+      dijkstra.min_distances(f.graph, onode, {:max_nodes => 1, :ignore_directions => true}).should == {
+        [1, :start_is_first]=>0,
+        [1, :end_is_first]=>0,
+        [2, :start_is_first]=>0,
+        [2, :end_is_first]=>0,
+        }
+      dijkstra.min_distances(f.graph, onode, {:max_nodes => 2, :ignore_directions => true}).should == {
+        [1, :start_is_first]=>0,
+        [1, :end_is_first]=>0,
+        [2, :start_is_first]=>0,
+        [2, :end_is_first]=>0,
+        [3, :start_is_first]=>10,
+        [3, :end_is_first]=>10
+        }
+    end
+
+    it 'should be ok with max-nodes when there is a tie' do
+      f = GraphTesting.finishm_graph([[1,2],[1,3]])
+      dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
+      onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
+
+      dijkstra.min_distances(f.graph, onode, {:max_nodes => 0, :ignore_directions => true}).should == {
+        [1, :start_is_first]=>0,
+        [1, :end_is_first]=>0,
+        }
+      dijkstra.min_distances(f.graph, onode, {:max_nodes => 1, :ignore_directions => true}).should == {
+        [1, :start_is_first]=>0,
+        [1, :end_is_first]=>0,
+        [2, :start_is_first]=>0,
+        [2, :end_is_first]=>0,
+        [3, :start_is_first]=>0,
+        [3, :end_is_first]=>0,
+        }
+
+      f = GraphTesting.finishm_graph([[10,1],[1,2],[1,3]])
+      dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
+      onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[10], :start_is_first)
+      dijkstra.min_distances(f.graph, onode, {:max_nodes => 2, :ignore_directions => true}).should == {
+        [10, :start_is_first]=>0,
+        [10, :end_is_first]=>0,
+        [1, :start_is_first]=>0,
+        [1, :end_is_first]=>0,
+        [2, :start_is_first]=>10,
+        [2, :end_is_first]=>10,
+        [3, :start_is_first]=>10,
+        [3, :end_is_first]=>10,
+        }
+    end
+
+    it 'should not get tripped up on negative distances' do
+      f = GraphTesting.finishm_graph([[1,2],[3,4]])
+      dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
+      onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
+      GraphTesting.add_reads_to_nodes(f, [[2,1],[3,2]])
+      GraphTesting.make_reads_paired(f)
+      GraphTesting.set_node_length(f.graph.nodes[2], 1000)
+      finder = Bio::FinishM::PairedEndNeighbourFinder.new(f, 100)
+      dijkstra.min_distances(f.graph, onode, {
+        :max_nodes => 1, :ignore_directions => true, :neighbour_finder => finder
+        }).should == {
+        [1, :start_is_first]=>0,
+        [1, :end_is_first]=>0,
+        [2, :start_is_first]=>0,
+        [2, :end_is_first]=>0,
+        }
+    end
   end
 
-  it 'should respect max-nodes' do
-    f = GraphTesting.finishm_graph([[1,2],[2,3],[3,4]])
-    dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
-    onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
-    dijkstra.min_distances(f.graph, onode).should == {
-      [1, :start_is_first]=>0,
-      [2, :start_is_first]=>0,
-      [3, :start_is_first]=>10,
-      [4, :start_is_first]=>20,
-      }
-    dijkstra.min_distances(f.graph, onode, :max_nodes => 2).should == {
-      [1, :start_is_first]=>0,
-      [2, :start_is_first]=>0,
-      [3, :start_is_first]=>10,
-      }
-    dijkstra.min_distances(f.graph, onode, :max_nodes => 0).should == {
-      [1, :start_is_first]=>0,
-      }
-    dijkstra.min_distances(f.graph, onode, :max_nodes => 3).should == {
-      [1, :start_is_first]=>0,
-      [2, :start_is_first]=>0,
-      [3, :start_is_first]=>10,
-      [4, :start_is_first]=>20,
-      }
-    dijkstra.min_distances(f.graph, onode, :max_nodes => 30).should == {
-      [1, :start_is_first]=>0,
-      [2, :start_is_first]=>0,
-      [3, :start_is_first]=>10,
-      [4, :start_is_first]=>20,
-      }
-  end
+  describe 'trail stats' do
+    it 'should return nodes grouped by distance' do
+      f = GraphTesting.finishm_graph([[1,2],[1,3],[2,3]])
+      dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
+      onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
+      dijkstra.trail_stats(f.graph, onode).should == {
+        0=>{
+          [1, :start_is_first]=>1,
+          [2, :start_is_first]=>1,
+          [3, :start_is_first]=>1,
+          },
+        10=>{
+          [3, :start_is_first]=>1
+          }
+        }
+    end
 
-  it 'should max-nodes when ignoring directions' do
-    f = GraphTesting.finishm_graph([[1,2],[2,3],[3,4]])
-    dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
-    onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
+    it 'should track node positional multiplicities' do
+      f = GraphTesting.finishm_graph([
+        [1,3],
+        [2,3],
+        [3,4],
+        [3,5],
+        [4,6],
+        [5,6],
+        [6,7]
+        ])
+      dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
+      onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[3], :start_is_first)
+      dijkstra.trail_stats(f.graph, onode).should == {
+        0=>{
+          [3, :start_is_first]=>1,
+          [4, :start_is_first]=>1,
+          [5, :start_is_first]=>1
+          },
+        10=>{
+          [6, :start_is_first]=>2,
+          },
+        20=>{
+          [7, :start_is_first]=>2,
+          }
+        }
+    end
 
-    dijkstra.min_distances(f.graph, onode, {:max_nodes => 0, :ignore_directions => true}).should == {
-      [1, :start_is_first]=>0,
-      [1, :end_is_first]=>0,
-      }
-    dijkstra.min_distances(f.graph, onode, {:max_nodes => 1, :ignore_directions => true}).should == {
-      [1, :start_is_first]=>0,
-      [1, :end_is_first]=>0,
-      [2, :start_is_first]=>0,
-      [2, :end_is_first]=>0,
-      }
-    dijkstra.min_distances(f.graph, onode, {:max_nodes => 2, :ignore_directions => true}).should == {
-      [1, :start_is_first]=>0,
-      [1, :end_is_first]=>0,
-      [2, :start_is_first]=>0,
-      [2, :end_is_first]=>0,
-      [3, :start_is_first]=>10,
-      [3, :end_is_first]=>10
-      }
-  end
+    it 'should deal with long divergent paths' do
+      f = GraphTesting.finishm_graph([
+        [1,2],
+        [1,3],
+        [2,4],
+        [3,4],
+        [4,5],
+        [5,6]
+        ])
+      dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
+      onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
+      f.graph.nodes[3].ends_of_kmers_of_node = 'T'*100
+      f.graph.nodes[5].ends_of_kmers_of_node = 'T'*100 #long paths
+      dijkstra.trail_stats(f.graph, onode).should == {
+        0=>{
+          [1, :start_is_first]=>1,
+          [2, :start_is_first]=>1,
+          [3, :start_is_first]=>1,
+          },
+        10=>{
+          [4, :start_is_first]=>1,
+          },
+        20=>{
+          [5, :start_is_first]=>1,
+          },
+        100=>{
+          [4, :start_is_first]=>1,
+          },
+        110=>{
+          [5, :start_is_first]=>1,
+          },
+        120=>{
+          [6, :start_is_first]=>1,
+          },
+        210=>{
+          [6, :start_is_first]=>1,
+          }
+        }
+    end
 
-  it 'should be ok with max-nodes when there is a tie' do
-    f = GraphTesting.finishm_graph([[1,2],[1,3]])
-    dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
-    onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
+    it 'should ignore directions' do
+      f = GraphTesting.finishm_graph([
+        [1,3],
+        [2,3],
+        [3,4]
+        ])
+      onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[3], :end_is_first)
+      dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
+      dijkstra.trail_stats(f.graph, onode, {
+        :ignore_directions => true, :max_nodes => 2
+        }).should == {
+        0=>{
+          [3, :start_is_first]=>1,
+          [3, :end_is_first]=>1,
+          [1, :start_is_first]=>2,
+          [1, :end_is_first]=>2,
+          [2, :start_is_first]=>2,
+          [2, :end_is_first]=>2
+          }
+        }
+    end
 
-    dijkstra.min_distances(f.graph, onode, {:max_nodes => 0, :ignore_directions => true}).should == {
-      [1, :start_is_first]=>0,
-      [1, :end_is_first]=>0,
-      }
-    dijkstra.min_distances(f.graph, onode, {:max_nodes => 1, :ignore_directions => true}).should == {
-      [1, :start_is_first]=>0,
-      [1, :end_is_first]=>0,
-      [2, :start_is_first]=>0,
-      [2, :end_is_first]=>0,
-      [3, :start_is_first]=>0,
-      [3, :end_is_first]=>0,
-      }
+    it 'should respect max nodes' do
+      f = GraphTesting.finishm_graph([
+        [1,3],
+        [2,3],
+        [3,4],
+        [4,5],
+        [4,6],
+        [5,6]
+        ])
+      onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[3], :start_is_first)
+      dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
+      dijkstra.trail_stats(f.graph, onode, { :max_nodes => 1 }).should == {
+        0=>{
+          [3, :start_is_first]=>1,
+          [4, :start_is_first]=>1
+          }
+        }
 
-    f = GraphTesting.finishm_graph([[10,1],[1,2],[1,3]])
-    dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
-    onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[10], :start_is_first)
-    dijkstra.min_distances(f.graph, onode, {:max_nodes => 2, :ignore_directions => true}).should == {
-      [10, :start_is_first]=>0,
-      [10, :end_is_first]=>0,
-      [1, :start_is_first]=>0,
-      [1, :end_is_first]=>0,
-      [2, :start_is_first]=>10,
-      [2, :end_is_first]=>10,
-      [3, :start_is_first]=>10,
-      [3, :end_is_first]=>10,
-      }
-  end
+      dijkstra.trail_stats(f.graph, onode, { :max_nodes => 2 }).should == {
+        0=>{
+          [3, :start_is_first]=>1,
+          [4, :start_is_first]=>1
+          },
+        10=>{
+          [5, :start_is_first]=>1,
+          [6, :start_is_first]=>1
+          }
+        }
 
-  it 'should not get tripped up on negative distances' do
-    f = GraphTesting.finishm_graph([[1,2],[3,4]])
-    dijkstra = Bio::AssemblyGraphAlgorithms::Dijkstra.new
-    onode = Bio::Velvet::Graph::OrientedNodeTrail::OrientedNode.new(f.graph.nodes[1], :start_is_first)
-    GraphTesting.add_reads_to_nodes(f, [[2,1],[3,2]])
-    GraphTesting.make_reads_paired(f)
-    GraphTesting.set_node_length(f.graph.nodes[2], 1000)
-    finder = Bio::FinishM::PairedEndNeighbourFinder.new(f, 100)
-    dijkstra.min_distances(f.graph, onode, {
-      :max_nodes => 1, :ignore_directions => true, :neighbour_finder => finder
-      }).should == {
-      [1, :start_is_first]=>0,
-      [1, :end_is_first]=>0,
-      [2, :start_is_first]=>0,
-      [2, :end_is_first]=>0,
-      }
+      dijkstra.trail_stats(f.graph, onode, { :max_nodes => 3 }).should == {
+        0=>{
+          [3, :start_is_first]=>1,
+          [4, :start_is_first]=>1
+          },
+        10=>{
+          [5, :start_is_first]=>1,
+          [6, :start_is_first]=>1
+          }
+        }
+
+      dijkstra.trail_stats(f.graph, onode, { :max_nodes => 4 }).should == {
+        0=>{
+          [3, :start_is_first]=>1,
+          [4, :start_is_first]=>1
+          },
+        10=>{
+          [5, :start_is_first]=>1,
+          [6, :start_is_first]=>1
+          },
+        20=>{
+          [6, :start_is_first]=>1
+          }
+        }
+    end
   end
 end
